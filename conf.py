69a4d8ab356e4bd1d17b40473d74f2db809e9ced
Aditya Mulgundkar <adi@svoot.com>
2020-01-24 20:51:00 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,6 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+master_doc = 'index'
 
 # -- Project information -----------------------------------------------------
 
@@ -27,8 +28,7 @@ author = 'Aditya Mulgundkar, Marmik Thakkar'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
